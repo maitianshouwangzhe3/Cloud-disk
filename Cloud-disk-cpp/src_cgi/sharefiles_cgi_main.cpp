93dfb27f4b159570e15063d3964db67b29098ee8
maitianshouwangzhe3 <2681457694@qq.com>
2025-02-05 21:23:50 +0800

    删除无用资源，准备重构

--- Cloud-disk-cpp/src_cgi/sharefiles_cgi_main.cpp
+++
@@ -1,9 +0,0 @@
-#include "sharefiles_cgi.h"
-
-int main(){
-    sharefiles sf;
-    sf.run();
-    return 0;
-}
-
-// g++ -o sharefiles sharefiles_cgi.cpp sharefiles_cgi_main.cpp ../common/cJSON.cpp ../common/redis_op.cpp ../common/deal_mysql.cpp ../common/make_log.c ../common/cfg.cpp ../common/util_cgi.cpp -lmysqlclient -lm -lfcgi -lhiredis -I /usr/local/include -L /usr/local/lib -lfcgi -lstdc++ -lfcgi++
